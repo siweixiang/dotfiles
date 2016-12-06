395e59a9f89e840ed4cbcd13c98a53fe396d3c71
irenicus <irenicus09@gmail.com>
2016-12-06 22:30:37 +0700

    fixed url copy/paste bug

--- getpic.sh
+++ getpic.sh
@@ -2,17 +2,16 @@
 # Script to get grab url from clipboard,
 # download file from url to /tmp directory using wget 
 # & displaying using native file handlers
-
+url=$(xclip -o);
 filename=$(date +%y%m%d%H%M%S);
-url=$(parcellite -c 2>/dev/null);
 wget -c --content-disposition $url -O /tmp/$filename;
 
 if [ $? -eq 0 ] 
 then 
-	/usr/bin/gpicview /tmp/$filename;
+    /usr/bin/gpicview /tmp/$filename;
 
 else 
-	exit 1;
+    exit 1;
 fi
 
 exit 0;
